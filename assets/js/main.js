0a32ef7db5d92a35064649785316764bf7e97539
ErrayAbdellah <mahdiabdllah121@gmail.com>
2022-12-25 15:09:54 +0100

    js is added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,57 +1,59 @@
-let indexQst = 0 ;
- let card1      = document.getElementById('card1');
- let card2      = document.getElementById('card2');
- let card3      = document.getElementById('card3');
- let card4      = document.getElementById('card4');
- let qstCard    = document.getElementById('text');
- let bottun     =  document.querySelector('.card');
+let card1      = document.getElementById('card1');
+let card2      = document.getElementById('card2');
+let card3      = document.getElementById('card3');
+let card4      = document.getElementById('card4');
+let qstCard    = document.getElementById('text');
+let bottun     =  document.querySelector('.card');
 
- 
- getQuestions();
- 
+
+getQuestions();
+let indexQst = 0 ;
  var light = document.getElementById('light');
+ var questionObjs =[];
+ let countQst = 0;
+
 
 
 
   // ajax get DATA
-  function getQuestions(index){
+  function getQuestions(){
     let aj = new XMLHttpRequest();
 
     aj.onreadystatechange = function(){
       if(this.readyState===4 && this.status===200){
-        let questionObjs = JSON.parse(this.responseText);
-        let countQst = questionObjs.length ;
-        //console.log(countQst);
-        addData(questionObjs[indexQst],countQst,index);
+         questionObjs = JSON.parse(this.responseText);
+         countQst = questionObjs.length ;
+        arrTest = questionObjs;       
+       getData();
       }
     }
-    
     aj.open("GET","../../questions.json",true);
     aj.send();
   }
   
-  function addData(question,count,index){
-    if(indexQst <count){
-      qstCard.innerText = question['question'];
-      card1.innerText   = question['choice1'];
-      card2.innerText   = question['choice2'];
-      card3.innerText   = question["choice3"];
-      card4.innerText   = question['choice4'];
-      let answer = question['answer'];
-      indexQst++;
-      // checkAnswer(answer,index);
+  function getData(){
+    
+    if(indexQst <countQst){
+      qstCard.innerText = questionObjs[indexQst]['question'];
+      card1.innerText   = questionObjs[indexQst]['choice1'];
+      card2.innerText   = questionObjs[indexQst]['choice2'];
+      card3.innerText   = questionObjs[indexQst]["choice3"];
+      card4.innerText   = questionObjs[indexQst]['choice4'];
+      let answer        = questionObjs[indexQst]['answer'] ;
+     
       return answer;
     }else{
       location.reload()
       alert("fin");
     }
   }
-  answer = addData();
-  console.log(answer);
+  
   function reponse(index)
   {
-    //getQuestions(index.index);
-    console.log(index.value)
+    getQuestions()
+    console.log("this is index : "+index.value)
+    console.log("htis is answert : "+getData());
+    test(getData(),index.value)
   }
   function test(question,index){
     console.log(question);
@@ -62,11 +64,10 @@ let indexQst = 0 ;
       }else{
         alert("false");
       }
-      
+      indexQst++;
   }
- 
-  function randomUniqueNum(Count) {
 
+  function randomUniqueNum(Count) {
     let arr = []
     for (let i = 1; i <= Count; i++) {
       arr.push(i)
