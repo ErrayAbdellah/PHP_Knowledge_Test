e0ce2ca2254e7ed22ab702ef22d2c6a162476c29
ErrayAbdellah <mahdiabdllah121@gmail.com>
2022-12-27 17:22:07 +0100

    time from main.js is added

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,8 @@ let card3      = document.getElementById('card3');
 let card4      = document.getElementById('card4');
 let qstCard    = document.getElementById('text');
 let bottun     =  document.querySelector('.card');
+let scoreResult = document.getElementById('score');
+let timer = document.querySelector('.timer');
 
 
 getQuestions();
@@ -27,41 +29,44 @@ let indexQst = 0 ;
          countQst = questionObjs.length ;
          countRandom = randomCount(countQst);
          arrTest = questionObjs;       
-       getData();
+         getData();
+        }
       }
+      aj.open("GET","../../questions.json",true);
+      aj.send();
     }
-    aj.open("GET","../../questions.json",true);
-    aj.send();
-  }
+    //  console.log(countRandom);
   
   function getData(){
-    //console.log()
     if(indexQst <countQst){
       getQuestions();
-      document.querySelector("#NmbrQst").innerText = indexQst+"/"+countQst;
+      document.querySelector("#NmbrQst").innerText = (indexQst+1)+"/"+countQst;
       qstCard.innerText = questionObjs[indexQst]['question'];
       card1.innerText   = questionObjs[indexQst]['choice1'];
       card2.innerText   = questionObjs[indexQst]['choice2'];
       card3.innerText   = questionObjs[indexQst]["choice3"];
       card4.innerText   = questionObjs[indexQst]['choice4'];
       let answer        = questionObjs[indexQst]['answer'] ;
+     
       return answer;
     }else{
       $(".question").css({'display':'none'});
       $(".info").css({'display':'none'});
       $(".result").css({'display':'block'});
       document.querySelector(".three").classList.add("active");
+      scoreResult.innerText = score+"/"+(countQst-1);
     }
   }
   
   function reponse(index)
   {
+    tm();
     getQuestions();
     test(getData(),index);
-
     setTimeout(() => {
       indexQst++;
     }, 500);
+    
   }
   
   function test(question,index){
@@ -72,7 +77,6 @@ let indexQst = 0 ;
       setTimeout(() => {
         $("#checking").text("");
       }, 500);
-      
       score++;
     }else{
       $("#checking").text("Incorrect !!!");
@@ -80,8 +84,7 @@ let indexQst = 0 ;
       setTimeout(() => {
         $("#checking").text("");
       }, 500);
-      }
-        
+      }        
   }
 
   function randomCount(Count) {
@@ -109,11 +112,29 @@ let indexQst = 0 ;
   $(".question").css({'display':'none'});
   $(".result").css({'display':'none'});
   // $(".info").css({'display':'none'});
-
+  
   function bntNext(){
     $(".question").css({'display':'block'});
     $(".info").css({'display':'none'});
     $(".result").css({'display':'none'});
     document.querySelector(".two").classList.add("active");
+    tm();
+    
   }
-  //console.log(randomUniqueNum(10))
+   //console.log(randomUniqueNum(10))
+   //tm();
+   function tm(){
+     let sec = 9;
+     let t = setInterval(tms,1000);
+     function tms(){
+        if(sec===-1)
+          {
+            sec = 9;
+            indexQst++;
+            clearInterval(t);
+          }else{
+            timer.innerHTML = '00:0'+sec;
+            sec --;
+          }
+     }
+    }
